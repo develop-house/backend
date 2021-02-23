8ed5d0c368d2afccd8e946a6e260cd7ed454f6b2
Seongjoo Jin <evanjin8@gmail.com>
2021-02-23 21:57:22 +0900

    핫로더, 스웨거 추가

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14
+FROM node:lts
 
 # Create app directory
 WORKDIR /usr/src/app
@@ -6,14 +6,19 @@ WORKDIR /usr/src/app
 # Install app dependencies
 # A wildcard is used to ensure both package.json AND package-lock.json are copied
 # where available (npm@5+)
-COPY package*.json ./
+COPY --chown=node:node package*.json ./
 
-RUN npm install
+# RUN rm package-lock.json
+RUN npm i -g @nestjs/cli
+
+RUN npm i
+# RUN npm run build
 # If you are building your code for production
 # RUN npm ci --only=production
+RUN npm cache clean --force
 
 # Bundle app source
 COPY . .
 
-EXPOSE 3000
+EXPOSE 13000
 CMD [ "npm", "run","start:dev" ]
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Devhouse')
+    .setDescription('The devhouse API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(13000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
