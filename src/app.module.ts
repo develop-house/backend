c3447b218b5b2d43f0909b62e67b381e9d88101b
evan <evanjin@toons.kr>
2021-02-25 17:22:40 +0900

    :seedling: init club table files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { ClubController } from './club/club.controller';
+import { ClubService } from './club/club.service';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://db_mongo'), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ClubController],
+  providers: [AppService, ClubService],
 })
 export class AppModule {}
