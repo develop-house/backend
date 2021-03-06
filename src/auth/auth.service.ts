d8f758e4046ab6d11e1b7a7fe5da142255f7e5da
MaengJowan <helpandplay.kr@gmail.com>
2021-03-07 02:03:28 +0900

    🎨 Fix auth.service logic
    - 플레인 텍스트를 메시지 의존성 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,32 +5,38 @@ import { User } from 'src/user/entity/user.entity';
 import { Repository } from 'typeorm';
 import { Login } from '../type/auth';
 import Bcrypt from './auth.bcrypt';
+import { Message, message } from './auth.message';
+import AuthVaildator from './auth.vaildator';
 
 @Injectable()
 export class AuthService {
   private bcrypt = new Bcrypt();
+  private authVaildator = new AuthVaildator();
   constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
 
   async login(login: Login): Promise<string> {
     const user = await this.userRepository.findOne(login.email);
-    if (!user) return 'unknown User';
+    if (!user) return message.user.noExisting;
 
-    const correctPassword = await this.bcrypt.toCompare(login.password, user.password);
-    if (!correctPassword) return 'not equal password';
-    return 'success';
+    const equalPassword = await this.bcrypt.toCompare(login.password, user.password);
+    if (!equalPassword) return message.password.notEqual;
+    return message.success;
   }
 
   async join(join: CreateUserDto): Promise<string> {
     const user = await this.userRepository.findOne(join.email);
-    if (user) return 'already existing email!';
+    if (user) return message.email.alreadyExisting;
+
+    const result: Message = await this.authVaildator.join(join);
+    if (result !== message.success) return result as string;
 
     const hashedPassword = await this.bcrypt.toHash(join.password);
     const createUser = await this.userRepository.save({
       ...join,
       password: hashedPassword,
     });
-    if (!createUser) return 'failed to create User';
+    if (!createUser) return message.user.failedJoin;
 
-    return 'Success!';
+    return message.success;
   }
 }
